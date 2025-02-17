b488ab5b1a17c5fc9d4e5941067ffec684e9061f
Ed Bennett <e.j.bennett@swansea.ac.uk>
2025-02-17 18:44:16 +0000

    fix bugs in certificate getter

--- gateway/get_certificate.sh
+++ gateway/get_certificate.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-if (( $? == 0 ))
+if (( $# == 0 ))
 then
   echo "Need at least one domain name." >/dev/stderr
   exit 1
@@ -14,4 +14,4 @@ do
   DOMAIN_CMD="-d ${DOMAIN} ${DOMAIN_CMD}"
 done
 
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d uklft-dg.swansea.ac.uk -d uklft-dg.swan.ac.uk -d uklft-dg.abertawe.ac.uk
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run ${DOMAIN_CMD}
